78823f2aadde235281520068c314cc124376a40e
TexasBall! <vishruthgm1@gmail.com>
2024-11-04 10:14:33 -0600

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,10 @@
+tup1 = (4, 3, 2, 2, -1, 18)
+tup2 = (2, 4, 8, 8, 3, 2, 9)
+product1 = 1
+product2 = 2
+for x in tup1:
+  product1 = product1*x
+print(product1)
+for num in tup2:
+  product2 = product2*num
+print(product2)
